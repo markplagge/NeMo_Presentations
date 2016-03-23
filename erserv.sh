5c37f1ba09243d647d745c289c5ea66c8de062e4
Mark Plagge <plaggm@rpi.edu>
2016-03-23 13:13:20 -0400

    near final version.

---
+++ erserv.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+erl -s inets -eval 'inets:start(httpd,[{server_name,"NAME"},{document_root, "."},{server_root, "."},{port, 8000},{mime_types,[{"html","text/html"},{"htm","text/html"},{"js","text/javascript"},{"css","text/css"},{"gif","image/gif"},{"jpg","image/jpeg"},{"jpeg","image/jpeg"},{"png","image/png"}]}]).'
---
+++ rvp/markdowns/weakscaling.md
@@ -0,0 +1,3 @@
+![](./result_img/weak_scaling_en_chart.png)<!-- .element: class="fill" width="40%" -->
+
+- NeMo shows excellent weak scaling performance
